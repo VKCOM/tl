fd55ec27875f6f4463353a8beb33e2d78c09c886
m.bordyugov <m.bordyugov@corp.vk.com>
2023-07-21 02:37:29 +0300

    remove redundant copyright claim

--- internal/tlpprof/profile/merge.go
+++ internal/tlpprof/profile/merge.go
@@ -1,9 +1,3 @@
-// Copyright 2022 V Kontakte LLC
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
 // Copyright 2019 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
