94837c71427d3fb853a86dc29facf0fcfafd4dd8
Fedor Vikhnin <fvixnin@gmail.com>
2025-04-14 16:17:12 +0300

    fixes 2

--- internal/tlcodegen/test/gen/schema_cpp/service1/headers/service1_dictionaryField.h
+++ internal/tlcodegen/test/gen/schema_cpp/service1/headers/service1_dictionaryField.h
@@ -3,6 +3,7 @@
 #include "../../basictl/io_streams.h"
 #include "../../basictl/io_throwable_streams.h"
 #include "../../__common_namespace/types/dictionaryField.h"
+#include "../types/service1.Value.h"
 
 namespace tl2 { namespace details { 
 
@@ -11,6 +12,26 @@ void BuiltinVectorDictionaryFieldDictionaryIntReset(std::map<std::string, std::m
 bool BuiltinVectorDictionaryFieldDictionaryIntWriteJSON(std::ostream & s, const std::map<std::string, std::map<std::string, int32_t>>& item);
 bool BuiltinVectorDictionaryFieldDictionaryIntRead(::basictl::tl_istream & s, std::map<std::string, std::map<std::string, int32_t>>& item);
 bool BuiltinVectorDictionaryFieldDictionaryIntWrite(::basictl::tl_ostream & s, const std::map<std::string, std::map<std::string, int32_t>>& item);
+
+}} // namespace tl2::details
+
+namespace tl2 { namespace details { 
+
+void BuiltinVectorDictionaryFieldService1ValueReset(std::map<std::string, ::tl2::service1::Value>& item);
+
+bool BuiltinVectorDictionaryFieldService1ValueWriteJSON(std::ostream & s, const std::map<std::string, ::tl2::service1::Value>& item);
+bool BuiltinVectorDictionaryFieldService1ValueRead(::basictl::tl_istream & s, std::map<std::string, ::tl2::service1::Value>& item);
+bool BuiltinVectorDictionaryFieldService1ValueWrite(::basictl::tl_ostream & s, const std::map<std::string, ::tl2::service1::Value>& item);
+
+}} // namespace tl2::details
+
+namespace tl2 { namespace details { 
+
+void BuiltinVectorDictionaryFieldStringReset(std::map<std::string, std::string>& item);
+
+bool BuiltinVectorDictionaryFieldStringWriteJSON(std::ostream & s, const std::map<std::string, std::string>& item);
+bool BuiltinVectorDictionaryFieldStringRead(::basictl::tl_istream & s, std::map<std::string, std::string>& item);
+bool BuiltinVectorDictionaryFieldStringWrite(::basictl::tl_ostream & s, const std::map<std::string, std::string>& item);
 
 }} // namespace tl2::details
 
