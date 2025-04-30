8f8a7a9fdacf07562247782495a79ac4f4b25db9
Fedor Vikhnin <fvixnin@gmail.com>
2025-04-30 18:16:31 +0300

    read basics

--- pkg/basictl/basictl2.go
+++ pkg/basictl/basictl2.go
@@ -6,46 +6,57 @@ import (
 	"math"
 )
 
-func TL2ReadSize(r []byte) (int, []byte, error) {
+func TL2UnexpectedByteError(actualByte, expectedByte byte) error {
+	return fmt.Errorf("unexpected byte %d, expected: %d", actualByte, expectedByte)
+}
+
+func TL2ExpectedZeroError() error {
+	return fmt.Errorf("expected zero value")
+}
+
+func TL2ExpectedNonZeroError() error {
+	return fmt.Errorf("expected non zero value")
+}
+
+func TL2ReadSize(r []byte, l *int) ([]byte, error) {
 	if len(r) == 0 {
-		return 0, r, io.ErrUnexpectedEOF
+		return r, io.ErrUnexpectedEOF
 	}
 	b0 := r[0]
 
-	var l int
 	switch {
 	case b0 <= tinyStringLen:
-		l = int(b0)
+		*l = int(b0)
 		r = r[1:]
 	case b0 == bigStringMarker:
 		if len(r) < 4 {
-			return 0, r, io.ErrUnexpectedEOF
+			return r, io.ErrUnexpectedEOF
 		}
-		l = (int(r[3]) << 16) + (int(r[2]) << 8) + (int(r[1]) << 0)
+		*l = (int(r[3]) << 16) + (int(r[2]) << 8) + (int(r[1]) << 0)
 		r = r[4:]
-		if l <= tinyStringLen {
-			return 0, r, fmt.Errorf("non-canonical (big) string format for length: %d", l)
+		if *l <= tinyStringLen {
+			return r, fmt.Errorf("non-canonical (big) string format for length: %d", l)
 		}
 	default: // hugeStringMarker
 		if len(r) < 8 {
-			return 0, r, io.ErrUnexpectedEOF
+			return r, io.ErrUnexpectedEOF
 		}
 		l64 := (int64(r[7]) << 48) + (int64(r[6]) << 40) + (int64(r[5]) << 32) + (int64(r[4]) << 24) + (int64(r[3]) << 16) + (int64(r[2]) << 8) + (int64(r[1]) << 0)
 		if l64 > math.MaxInt {
-			return 0, r, fmt.Errorf("string length cannot be represented on 32-bit platform: %d", l64)
+			return r, fmt.Errorf("string length cannot be represented on 32-bit platform: %d", l64)
 		}
-		l = int(l64)
+		*l = int(l64)
 		r = r[8:]
-		if l <= bigStringLen {
-			return 0, r, fmt.Errorf("non-canonical (huge) string format for length: %d", l)
+		if *l <= bigStringLen {
+			return r, fmt.Errorf("non-canonical (huge) string format for length: %d", l)
 		}
 	}
-	if l > 0 {
-		if len(r) < l {
-			return 0, r, io.ErrUnexpectedEOF
+	if *l > 0 {
+		if len(r) < *l {
+			return r, io.ErrUnexpectedEOF
 		}
 	}
-	return l, r, nil
+	return r, nil
 }
 
 func TL2WriteSize(w []byte, l int) []byte {
@@ -84,4 +95,21 @@ func StringBytesWriteTL2(w []byte, v []byte) []byte {
 	w = TL2WriteSize(w, len(v))
 	w = append(w, v...)
 	return w
+}
+
+func ByteReadTL2(r []byte, b *byte) ([]byte, error) {
+	if len(r) == 0 {
+		return nil, io.ErrUnexpectedEOF
+	}
+	*b = r[0]
+	return r[1:], nil
+}
+
+func BoolReadTL2(r []byte, b *bool) (_ []byte, err error) {
+	var byt byte
+	if r, err = ByteReadTL2(r, &byt); err != nil {
+		return r, err
+	}
+	*b = byt == 1
+	return r, err
 }
