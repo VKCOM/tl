a4a3f4d8c7af7b6f86126385d3dfc7c0183d67d9
Fedor Vikhnin <fvixnin@gmail.com>
2025-05-03 02:01:47 +0300

    new tests

---
+++ internal/tlcodegen/test/codegen_test/golang/casetests/bytes_read_tl2_benchmark_test.go
@@ -0,0 +1,109 @@
+package casetests
+
+import (
+	"github.com/vkcom/tl/internal/tlcodegen/test/gen/cases/meta"
+	"github.com/vkcom/tl/internal/tlcodegen/test/gen/cases/tlbenchmarks"
+	"github.com/vkcom/tl/pkg/basictl"
+	"math/rand"
+	"testing"
+)
+
+const seed int64 = 11234567
+
+func initTestValues[T meta.Object](tlVersion int, constructor func() T) func(iteration int) []byte {
+	r := basictl.NewRandGenerator(rand.New(rand.NewSource(seed)))
+
+	return func(iteration int) []byte {
+		testObject := constructor()
+		testObject.FillRandom(r)
+
+		var bytes []byte
+		var err error
+
+		if tlVersion == 1 {
+			bytes, err = testObject.WriteGeneral(nil)
+			if err != nil {
+				panic("can't init such data")
+			}
+		} else {
+			bytes, _ = testObject.WriteTL2(nil, nil)
+		}
+		return bytes
+	}
+}
+
+func BenchmarkTL1ReadRandomVector(b *testing.B) {
+	b.ReportAllocs()
+
+	valueGen := initTestValues[*tlbenchmarks.VrutoyTopLevelContainer](1, func() *tlbenchmarks.VrutoyTopLevelContainer {
+		return &tlbenchmarks.VrutoyTopLevelContainer{}
+	})
+	dst := tlbenchmarks.VrutoyTopLevelContainer{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		value := valueGen(i)
+		b.StartTimer()
+		_, err := dst.Read(value)
+		if err != nil {
+			b.Fail()
+		}
+	}
+}
+
+func BenchmarkTL2ReadRandomVector(b *testing.B) {
+	b.ReportAllocs()
+
+	valueGen := initTestValues[*tlbenchmarks.VrutoyTopLevelContainer](2, func() *tlbenchmarks.VrutoyTopLevelContainer {
+		return &tlbenchmarks.VrutoyTopLevelContainer{}
+	})
+	dst := tlbenchmarks.VrutoyTopLevelContainer{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		value := valueGen(i)
+		b.StartTimer()
+		_, err := dst.ReadTL2(value)
+		if err != nil {
+			b.Fail()
+		}
+	}
+}
+
+func BenchmarkTL1ReadRandomArray(b *testing.B) {
+	b.ReportAllocs()
+
+	valueGen := initTestValues[*tlbenchmarks.VrutoyTopLevelContainerWithDependency](1, func() *tlbenchmarks.VrutoyTopLevelContainerWithDependency {
+		return &tlbenchmarks.VrutoyTopLevelContainerWithDependency{}
+	})
+	dst := tlbenchmarks.VrutoyTopLevelContainerWithDependency{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		value := valueGen(i)
+		b.StartTimer()
+		_, err := dst.Read(value)
+		if err != nil {
+			b.Fail()
+		}
+	}
+}
+
+func BenchmarkTL2ReadRandomArray(b *testing.B) {
+	b.ReportAllocs()
+
+	valueGen := initTestValues[*tlbenchmarks.VrutoyTopLevelContainerWithDependency](2, func() *tlbenchmarks.VrutoyTopLevelContainerWithDependency {
+		return &tlbenchmarks.VrutoyTopLevelContainerWithDependency{}
+	})
+	dst := tlbenchmarks.VrutoyTopLevelContainerWithDependency{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		value := valueGen(i)
+		b.StartTimer()
+		_, err := dst.ReadTL2(value)
+		if err != nil {
+			b.Fail()
+		}
+	}
+}
