f1a9e931d0152eefbc265a10090c303a4fbc9c3d
Fedor Vikhnin <fvixnin@gmail.com>
2025-11-25 16:49:43 +0300

    fix

--- internal/tlcodegen/helpers_cpp_generated.go
+++ internal/tlcodegen/helpers_cpp_generated.go
@@ -173,7 +173,7 @@ namespace basictl {
 
     tl_istream::tl_istream(tl_throwable_istream &from) : provider(nullptr) {
         from.pass_data(*this);
-        this->sync_in_destructor = true;
+        this->sync_in_destructor = false;
     }
 
     bool tl_istream::string_read(std::string &value) noexcept {
