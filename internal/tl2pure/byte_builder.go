0a890f8787923471390caf2faa752e93bb9b8b69
Hrissan <hrissan@mail.ru>
2025-12-30 00:13:34 +0300

    buf with colors

--- internal/tl2pure/byte_builder.go
+++ internal/tl2pure/byte_builder.go
@@ -2,8 +2,17 @@ package tl2pure
 
 import "github.com/vkcom/tl/pkg/basictl"
 
+type ByteColor byte
+
+const ByteColorNormal = 0
+const ByteColorObjectSize = 1
+const ByteColorElementCount = 2
+const ByteColorVariantIndex = 3
+const ByteColorFieldMask = 4
+
 type ByteBuilder struct {
-	buf []byte
+	buf    []byte
+	colors []ByteColor
 }
 
 func (b *ByteBuilder) Buf() []byte {
@@ -14,35 +23,61 @@ func (b *ByteBuilder) Len() int {
 	return len(b.buf)
 }
 
-func (b *ByteBuilder) Append(w ...byte) {
-	b.buf = append(b.buf, w...)
+// primitives write directly into buf, so we call it at the start of each our fun
+func (b *ByteBuilder) fixColors(c ByteColor) {
+	if len(b.colors) > len(b.buf) {
+		panic("color markers must never be ahead of bytes")
+	}
+	for len(b.colors) < len(b.buf) {
+		b.colors = append(b.colors, c)
+	}
+}
+
+func (b *ByteBuilder) WriteFieldmask() {
+	b.fixColors(ByteColorNormal)
+	b.buf = append(b.buf, 0)
+	b.fixColors(ByteColorFieldMask)
 }
 
 func (b *ByteBuilder) WriteString(v string) {
-	b.buf = basictl.StringWriteTL2(b.buf, v)
+	b.fixColors(ByteColorNormal)
+	b.buf = basictl.TL2WriteSize(b.buf, len(v))
+	b.fixColors(ByteColorObjectSize)
+	b.buf = append(b.buf, v...)
+	b.fixColors(ByteColorNormal)
 }
 
 func (b *ByteBuilder) WriteVariantIndex(v int) {
+	b.fixColors(ByteColorNormal)
 	b.buf = basictl.TL2WriteSize(b.buf, v)
+	b.fixColors(ByteColorVariantIndex)
 }
 
 func (b *ByteBuilder) WriteElementCount(v int) {
+	b.fixColors(ByteColorNormal)
 	b.buf = basictl.TL2WriteSize(b.buf, v)
+	b.fixColors(ByteColorElementCount)
 }
 
 func (b *ByteBuilder) ReserveSpaceForSize() int {
+	b.fixColors(ByteColorNormal)
 	b.buf = append(b.buf, make([]byte, 16)...)
+	b.fixColors(ByteColorObjectSize) // mark all bytes as size for simplicity of FinishSize
 	return len(b.buf)
 }
 
 func (b *ByteBuilder) FinishSize(firstUsedByte int, lastUsedByte int, optimizeEmpty bool) {
+	b.fixColors(ByteColorNormal)
 	if optimizeEmpty && firstUsedByte == lastUsedByte {
 		b.buf = b.buf[:firstUsedByte-16]
+		b.colors = b.colors[:firstUsedByte-16]
 		return
 	}
 	offset := basictl.TL2PutSize(b.buf[firstUsedByte-16:], lastUsedByte-firstUsedByte)
 	offset += copy(b.buf[firstUsedByte-16+offset:], b.buf[firstUsedByte:lastUsedByte])
+	copy(b.colors[firstUsedByte-16+offset:], b.colors[firstUsedByte:lastUsedByte])
 	b.buf = b.buf[:firstUsedByte-16+offset]
+	b.colors = b.colors[:firstUsedByte-16+offset]
 }
 
 // at current offset
