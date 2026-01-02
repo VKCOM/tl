89d1cadc2a5e6fa974126fc1d33e5263a0d1bf6f
Hrissan <hrissan@mail.ru>
2026-01-02 23:05:06 +0300

    ui work

--- internal/tl2pure/ui.go
+++ internal/tl2pure/ui.go
@@ -2,6 +2,7 @@ package tl2pure
 
 import (
 	"fmt"
+	"runtime/debug"
 	"strings"
 
 	"github.com/TwiN/go-color"
@@ -50,57 +51,61 @@ func (m *UIModel) colorError(str string) string {
 	return color.InRed(str)
 }
 
+func TLGenVersion() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		return info.Main.Version
+	}
+	return ""
+}
+
 func (m *UIModel) View() string {
 	var sb strings.Builder
+	version := ""
+	if info, ok := debug.ReadBuildInfo(); ok {
+		version = info.Main.Version
+	}
+	sb.WriteString(fmt.Sprintf("tl2client %s: ", version))
 	sb.WriteString(m.colorButton("[F1]"))
-	sb.WriteString(m.colorButtonComment("Legend"))
-	sb.WriteString(m.colorButton("[Tab]"))
-	sb.WriteString(m.colorButtonComment("Next"))
-	sb.WriteString(m.colorButton("[Shift-Tab]"))
-	sb.WriteString(m.colorButtonComment("Prev"))
-	sb.WriteString(m.colorButton("[<-/->]"))
-	sb.WriteString(m.colorButtonComment("Move"))
-	sb.WriteString(m.colorButton("[Enter]"))
-	sb.WriteString(m.colorButtonComment("Edit"))
-	sb.WriteString(m.colorButton("[Esc]"))
-	sb.WriteString(m.colorButtonComment("Cancel"))
-	sb.WriteString(m.colorButton("[F5]"))
-	sb.WriteString(m.colorButtonComment("Send"))
-	//sb.WriteString("F8")
-	//sb.WriteString(m.colorButtonComment("Extra"))
-	//sb.WriteString(m.colorButton("[F2]"))
-	//sb.WriteString(m.colorButtonComment("Show empty "))
+	sb.WriteString(m.colorButtonComment("Help"))
 	sb.WriteString("\n")
+	var bb ByteBuilder
+	if m.ShowLegend {
+		sb.WriteString(m.colorButton("[Tab]"))
+		sb.WriteString(m.colorButtonComment("Next"))
+		sb.WriteString(m.colorButton("[Shift-Tab]"))
+		sb.WriteString(m.colorButtonComment("Prev"))
+		sb.WriteString(m.colorButton("[<-/->]"))
+		sb.WriteString(m.colorButtonComment("Move"))
+		sb.WriteString(m.colorButton("[Enter]"))
+		sb.WriteString(m.colorButtonComment("Edit"))
+		sb.WriteString(m.colorButton("[Esc]"))
+		sb.WriteString(m.colorButtonComment("Cancel"))
+		sb.WriteString(m.colorButton("[F8]"))
+		sb.WriteString(m.colorButtonComment("Delete"))
+		sb.WriteString(m.colorButton("[F5]"))
+		sb.WriteString(m.colorButtonComment("Send"))
+		sb.WriteString("\n")
+		sb.WriteString("TL2 legend: ")
+		sb.WriteString(bb.PrintLegend())
+		sb.WriteString("\n")
+	}
+	//sb.WriteString(m.colorButtonComment("Extra"))
 	sb.WriteString(m.Fun.instance.canonicalName)
 	if m.Fun.instance.comb.FuncDecl.Magic != 0 {
 		sb.WriteString("#")
 		sb.WriteString(fmt.Sprintf("%08x", m.Fun.instance.comb.FuncDecl.Magic))
 	}
-	//sb.WriteString(" => ")
-	//sb.WriteString(m.Fun.instance.resultType.Combinator().TypeDecl.Type.St.CanonicalName())
 	sb.WriteString("    ")
 	if m.LastError != nil {
 		sb.WriteString(m.colorError(m.LastError.Error()))
 	}
+	//sb.WriteString(" => ")
+	//sb.WriteString(m.Fun.instance.resultType.Combinator().TypeDecl.Type.St.CanonicalName())
 	sb.WriteString("\n")
 
 	m.Fun.UIWrite(&sb, true, 0, m)
 	sb.WriteString("\n")
-	//TODO - do not take into account color characters
-	//var sb2 strings.Builder
-	//m.Fun.UIWrite(&sb2, true, 0, m.Path, m)
-	//str := sb2.String()
-	//for m.Width > 0 && len(str) > m.Width {
-	//	sb.WriteString(str[:m.Width])
-	//	sb.WriteString("\n")
-	//	str = str[m.Width:]
-	//}
-	//sb.WriteString(str)
-	var bb ByteBuilder
-	if m.ShowLegend {
-		sb.WriteString(bb.PrintLegend())
-		sb.WriteString("\n")
-	}
+
 	m.Fun.WriteTL2(&bb, false, true, 0, m)
 	sb.WriteString(bb.Print())
 	sb.WriteString("\n")
