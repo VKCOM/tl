efd14153fd855a1bddb7a75597b192005b40025c
Grigory Buteyko <g.buteyko@corp.vk.com>
2026-01-09 12:32:00 +0300

    tiny refactor

--- internal/tl2pure/t_alias.go
+++ internal/tl2pure/t_alias.go
@@ -2,8 +2,6 @@ package tl2pure
 
 import (
 	"fmt"
-	"math/rand"
-	"strings"
 
 	"github.com/vkcom/tl/internal/tlast"
 )
@@ -12,13 +10,6 @@ type TypeInstanceAlias struct {
 	TypeInstanceCommon
 	fieldType *TypeInstanceRef
 }
-
-type KernelValueAlias struct {
-	instance *TypeInstanceAlias
-	value    KernelValue
-}
-
-var _ KernelValue = &KernelValueAlias{}
 
 func (ins *TypeInstanceAlias) GoodForMapKey() bool {
 	return ins.fieldType.ins.GoodForMapKey()
@@ -46,53 +37,6 @@ func (ins *TypeInstanceAlias) CreateValue() KernelValue {
 
 func (ins *TypeInstanceAlias) SkipTL2(r []byte) ([]byte, error) {
 	return ins.fieldType.ins.SkipTL2(r)
-}
-
-func (v *KernelValueAlias) Clone() KernelValue {
-	return v.value.Clone()
-}
-
-func (v *KernelValueAlias) Reset() {
-	v.value.Reset()
-}
-
-func (v *KernelValueAlias) Random(rg *rand.Rand) {
-	v.value.Random(rg)
-}
-
-func (v *KernelValueAlias) WriteTL2(w *ByteBuilder, optimizeEmpty bool, onPath bool, level int, model *UIModel) {
-	v.value.WriteTL2(w, optimizeEmpty, onPath, level, model)
-}
-
-func (v *KernelValueAlias) ReadTL2(r []byte, ctx *TL2Context) ([]byte, error) {
-	return v.value.ReadTL2(r, ctx)
-}
-
-func (v *KernelValueAlias) WriteJSON(w []byte, ctx *TL2Context) []byte {
-	return v.value.WriteJSON(w, ctx)
-}
-
-func (v *KernelValueAlias) UIWrite(sb *strings.Builder, onPath bool, level int, model *UIModel) {
-	v.value.UIWrite(sb, onPath, level, model)
-}
-
-func (v *KernelValueAlias) UIFixPath(side int, level int, model *UIModel) int {
-	return v.value.UIFixPath(side, level, model)
-}
-
-func (v *KernelValueAlias) UIStartEdit(level int, model *UIModel, createMode int) {
-	v.value.UIStartEdit(level, model, createMode)
-}
-
-func (v *KernelValueAlias) UIKey(level int, model *UIModel, insert bool, delete bool, up bool, down bool) {
-	v.value.UIKey(level, model, insert, delete, up, down)
-}
-
-func (v *KernelValueAlias) CompareForMapKey(other KernelValue) int {
-	if v2, ok := other.(*KernelValueAlias); ok {
-		return v.value.CompareForMapKey(v2.value)
-	}
-	return 0
 }
 
 func (k *Kernel) createAlias(canonicalName string, alias tlast.TL2TypeRef,
---
+++ internal/tl2pure/t_alias_value.go
@@ -0,0 +1,60 @@
+package tl2pure
+
+import (
+	"math/rand"
+	"strings"
+)
+
+type KernelValueAlias struct {
+	instance *TypeInstanceAlias
+	value    KernelValue
+}
+
+var _ KernelValue = &KernelValueAlias{}
+
+func (v *KernelValueAlias) Clone() KernelValue {
+	return v.value.Clone()
+}
+
+func (v *KernelValueAlias) Reset() {
+	v.value.Reset()
+}
+
+func (v *KernelValueAlias) Random(rg *rand.Rand) {
+	v.value.Random(rg)
+}
+
+func (v *KernelValueAlias) WriteTL2(w *ByteBuilder, optimizeEmpty bool, onPath bool, level int, model *UIModel) {
+	v.value.WriteTL2(w, optimizeEmpty, onPath, level, model)
+}
+
+func (v *KernelValueAlias) ReadTL2(r []byte, ctx *TL2Context) ([]byte, error) {
+	return v.value.ReadTL2(r, ctx)
+}
+
+func (v *KernelValueAlias) WriteJSON(w []byte, ctx *TL2Context) []byte {
+	return v.value.WriteJSON(w, ctx)
+}
+
+func (v *KernelValueAlias) UIWrite(sb *strings.Builder, onPath bool, level int, model *UIModel) {
+	v.value.UIWrite(sb, onPath, level, model)
+}
+
+func (v *KernelValueAlias) UIFixPath(side int, level int, model *UIModel) int {
+	return v.value.UIFixPath(side, level, model)
+}
+
+func (v *KernelValueAlias) UIStartEdit(level int, model *UIModel, createMode int) {
+	v.value.UIStartEdit(level, model, createMode)
+}
+
+func (v *KernelValueAlias) UIKey(level int, model *UIModel, insert bool, delete bool, up bool, down bool) {
+	v.value.UIKey(level, model, insert, delete, up, down)
+}
+
+func (v *KernelValueAlias) CompareForMapKey(other KernelValue) int {
+	if v2, ok := other.(*KernelValueAlias); ok {
+		return v.value.CompareForMapKey(v2.value)
+	}
+	return 0
+}
