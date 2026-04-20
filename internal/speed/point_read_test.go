0ed51b31b1842fe4ce2ec3dbed27a811bbd55dec
Fedor Vikhnin <fvixnin@gmail.com>
2026-04-20 22:27:19 +0300

    add gen read

--- internal/speed/point_read_test.go
+++ internal/speed/point_read_test.go
@@ -3,7 +3,10 @@ package speed
 import (
 	"testing"
 
+	"github.com/VKCOM/tl/internal/speed/speed_proto/pb"
+	"github.com/VKCOM/tl/internal/speed/speed_tl/tl"
 	"google.golang.org/protobuf/encoding/protowire"
+	"google.golang.org/protobuf/proto"
 
 	"github.com/VKCOM/tl/pkg/basictl"
 )
@@ -36,6 +39,34 @@ func BenchmarkPointReadTL(b *testing.B) {
 	printSizes(b, int64(len(buf))*int64(finish))
 }
 
+func BenchmarkPointReadTLGen(b *testing.B) {
+	values, buf, _ := prepareTLPointsBuffer()
+	for _, v := range values {
+		buf = v.WriteTL1(buf)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	finish := b.N / len(values)
+	for i := 0; i < finish; i++ {
+		buf2 := buf
+		for _, v := range values {
+			var err error
+			var p tl.Point
+			buf2, err = p.ReadTL1(buf2)
+			if err != nil {
+				b.Fatalf("bad")
+			}
+			if p != v {
+				b.Fatalf("bad")
+			}
+		}
+		if len(buf2) != 0 {
+			b.Fatalf("bad")
+		}
+	}
+	printSizes(b, int64(len(buf))*int64(finish))
+}
+
 func BenchmarkPointReadTL2(b *testing.B) {
 	values, buf := preparePointsBuffer()
 	for _, v := range values {
@@ -50,6 +81,34 @@ func BenchmarkPointReadTL2(b *testing.B) {
 			var err error
 			var p point
 			buf2, err = p.readTL2(buf2)
+			if err != nil {
+				b.Fatalf("bad")
+			}
+			if p != v {
+				b.Fatalf("bad")
+			}
+		}
+		if len(buf2) != 0 {
+			b.Fatalf("bad")
+		}
+	}
+	printSizes(b, int64(len(buf))*int64(finish))
+}
+
+func BenchmarkPointReadTL2Gen(b *testing.B) {
+	values, buf, ctx := prepareTLPointsBuffer()
+	for _, v := range values {
+		buf = v.WriteTL2(buf, &ctx)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	finish := b.N / len(values)
+	for i := 0; i < finish; i++ {
+		buf2 := buf
+		for _, v := range values {
+			var err error
+			var p tl.Point
+			buf2, err = p.ReadTL2(buf2, nil)
 			if err != nil {
 				b.Fatalf("bad")
 			}
@@ -227,6 +286,39 @@ func BenchmarkPointReadProtobuf(b *testing.B) {
 				b.Fatalf("bad")
 			}
 			if res != v {
+				b.Fatalf("bad")
+			}
+		}
+		if len(buf2) != 0 {
+			b.Fatalf("bad")
+		}
+	}
+	printSizes(b, int64(len(buf))*int64(finish))
+}
+
+func BenchmarkPointReadProtobufGen(b *testing.B) {
+	values, buf := prepareProtoPointsBuffer()
+	for v := range values {
+		buf = protobufAppendPoint(buf, &point{x: values[v].X, y: values[v].Y, z: values[v].Z}, writeExcessField)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	finish := b.N / len(values)
+	for i := 0; i < finish; i++ {
+		buf2 := buf
+		for v := range values {
+			var err error
+			var res pb.PointPB // reuse
+			str, n := protowire.ConsumeBytes(buf2)
+			if n < 0 {
+				b.Fatalf("bad")
+			}
+			buf2 = buf2[n:]
+			err = proto.UnmarshalOptions{}.Unmarshal(str, &res)
+			if err != nil {
+				b.Fatalf("bad")
+			}
+			if res.X != values[v].X || res.Y != values[v].Y || res.Z != values[v].Z {
 				b.Fatalf("bad")
 			}
 		}
